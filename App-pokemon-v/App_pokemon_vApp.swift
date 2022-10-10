ba47c12c57cfc3934b87dca2fb1207c5e77c2bc5
Valéria Gonçalez <valeria.goncalez@aluno.ifsp.edu.br>
2022-10-10 20:10:55 -0300

    Initial Commit

---
+++ App-pokemon-v/App_pokemon_vApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_pokemon_vApp.swift
+//  App-pokemon-v
+//
+//  Created by user on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct App_pokemon_vApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App-pokemon-v/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App-pokemon-v
+//
+//  Created by user on 10/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
